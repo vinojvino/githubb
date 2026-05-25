761385bfff96a8d6fc0305aae53a3211274573be
vinojvino <vinojvino2004@gmail.com>
2026-05-25 11:52:00 +0530

    testing git bash

---
+++ oprators.py
@@ -0,0 +1,12 @@
+house_a=["table","rooms","walls"]
+house_b=["table","rooms","walls"]
+print(house_a==house_b)
+print(house_a is house_b)
+print(id(house_a))
+print(id(house_b))
+
+price=("99")
+print(type(price))
+price=int(price)
+print(price)
+print(type(price))
