2176ca55d848050576ee39ae6b949eb2a180a2bb
vinojvino <vinojvino2004@gmail.com>
2026-05-26 11:56:11 +0530

    test

--- learn_git.py
+++ learn_git.py
@@ -1,5 +1,17 @@
+#git config --global user.name "your name"
+#git config --global user.email "your email"
 #git int (choose route file)
 #git add.
 #git add filename.py
 #git commit -m "reason" (after making changes in vc code)
-#
+#git remote add orgin (link paste)
+#git branch -m master (pre exsisting branch)
+#git push orgin master
+
+
+#git clone
+#git pull
+
+#git branch branch name (create a new branch)
+#git hub
+
